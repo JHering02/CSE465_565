b22573f288d843f82811a1fe64dbd9d49b42e349
jHering02 <heringja@miamioh.edu>
2024-02-07 09:15:45 -0500

    comments

--- Homework1/src/ZpmReader.java
+++ Homework1/src/ZpmReader.java
@@ -10,12 +10,15 @@ import java.io.File;
  * and can also read loops. The interpreter will also detect runtime errors  
  */
 public class ZpmReader {
+    // private instance variable containing the filename
     private String fileName;
-
+    
+    // Basic parameter constructor
     public ZpmReader(String fileName) {
         this.fileName = fileName;
     }
 
+    // Use regex to follow specific zpm format
     public void read() {
         System.out.println("Reading file: " + fileName);
     }
