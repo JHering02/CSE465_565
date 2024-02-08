74bf01b82091c96087000786b0697f66c8c9b039
JHering02 <heringja@miamioh.edu>
2024-02-08 14:46:14 -0500

    Assign is tested and works, print implemented

--- Homework1/src/ZpmReader.java
+++ Homework1/src/ZpmReader.java
@@ -1,6 +1,7 @@
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -21,7 +22,7 @@ public class ZpmReader {
 
     // private map instance variable that contains all saved variables during
     // runtime
-    private Map<String, Object> variables;
+    private Map<String, Object> variables = new HashMap<String, Object>();
 
     // Basic parameter constructor
     public ZpmReader(String fileName) throws IOException {
@@ -35,28 +36,54 @@ public class ZpmReader {
         // Handle a for loop in zpm
     }
 
-    private void handlePrint() {
+    private void handlePrint(String outString) {
         // Handle a print statement in zpm
+        String output = outString.contains("\"") ? outString.replace("\"", "") : 
+        outString + "=" + variables.get(outString).toString();
+        System.out.println(output);
     }
 
     // Handle a simple assignment operator in zpm
-    private void assign(String varName, String opr, String value) {
+    private void assign(String varName, String opr, String value) throws Exception {
+        if (opr.equals("=")) {
+            Object newVal = variables.containsKey(value) ? variables.get(value) :  value;
+            newVal = !value.contains("\"") ? (int) Integer.parseInt(value) : (String) newVal.toString().replace("\"", "");
+            variables.put(varName, newVal);
+        } else if (variables.containsKey(varName) && variables.get(varName) instanceof Integer) {
+            int val = (int) variables.get(varName);
+            int newVal = variables.containsKey(value) ? (int) variables.get(value) :  Integer.parseInt(value);
+            if (opr.equals("+=")) {
+                variables.put(varName, val + newVal);
+            } else if (opr.equals("-=")) {
+                variables.put(varName, val - newVal);
+            } else if (opr.equals("*=")) {
+                variables.put(varName, val * newVal);
+            }
+        } else if (variables.containsKey(varName) && variables.get(varName) instanceof String && opr.equals("+=")) {
+            // Perform string concatenation if the variable is in the list and is a string
+            String val = (String) variables.get(varName);
+            variables.put(varName, val + value);
+        } else {
+            throw new Exception("Bad Syntax: " + varName + opr + value);
+        }
     }
 
-    private void interpret(String[] dat) {
+    private void interpret(String[] dat) throws Exception {
         int curr = 0;
-        do {
+        while (!dat[curr].equals(";")) {
             // check if the current token is a variable (regex A-Z only)
             if (dat[curr].matches("[A-Z]+") && dat[curr + 1].contains("=")) {
                 // check if the next token is an assignment operator
                 assign(dat[curr], dat[curr + 1], dat[curr + 2]);
+                curr += 3;
             } else if (dat[curr].equals("FOR")) {
                 handleFor();
             } else if (dat[curr].equals("PRINT")) {
-                handlePrint();
+                handlePrint(dat[curr + 1]);
+            } else {
+                throw new Exception("Invalid token: " + dat[curr]);
             }
-            curr++;
-        } while (dat[curr] != ";");
+        }
     }
 
     // Method that reads the zpm file and interprets the code
@@ -66,7 +93,7 @@ public class ZpmReader {
             try {
                 interpret(line.split(" "));
             } catch (Exception e) {
-                System.out.println("RUNTIME ERROR: line " + lineList.indexOf(line));
+                System.out.println("RUNTIME ERROR: line " + (lineList.indexOf(line) + 1));
                 System.exit(1);
             }
         }
